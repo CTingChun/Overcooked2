ab18b2f346502dc0231784532aaabb0739a415fe
106062104 <peter880609@gmail.com>
2019-06-21 21:46:46 +0800

    change scale

--- public/Player/index.js
+++ public/Player/index.js
@@ -22,10 +22,10 @@ class Player {
 
     // Move Animation
     this.sprite.frame = 8;
-    this.sprite.animations.add('right', [12, 13, 14, 15, 14, 13], 8, true);
-    this.sprite.animations.add('left', [4, 5, 6, 7, 6, 5], 8, true);
-    this.sprite.animations.add('up', [0, 1, 2, 3, 2, 1], 8, true);
-    this.sprite.animations.add('down', [8, 9, 10, 11, 10, 9], 8, true);
+    this.sprite.animations.add('right', [12, 13, 14, 15], 8, true);
+    this.sprite.animations.add('left', [4, 5, 6, 7], 8, true);
+    this.sprite.animations.add('up', [0, 1, 2, 3], 8, true);
+    this.sprite.animations.add('down', [8, 9, 10, 11], 8, true);
 
     //worldBound
     this.sprite.body.collideWorldBounds = true;
