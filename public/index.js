9d662e29babafd645b0faab7758d435fef5f407d
106062104 <peter880609@gmail.com>
2019-06-22 08:47:29 +0800

    final check

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ var game = new Phaser.Game(WindowWidth, WindowHeight, Phaser.AUTO);
 
 // Add Socket to Game
 game.socket = io('http://localhost:80/Hall');
-game.socket = io('http://192.168.50.133:80/Hall');
+//game.socket = io('http://192.168.50.133:80/Hall');
 
 // Add Game State
 game.state.add('Menu', new Menu());
