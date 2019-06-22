38be2e80b10c6e9cd21c8e7f3db4be4e1bade1de
Elven Lin <elvenlin1998@gmail.com>
2019-06-22 08:06:51 +0800

    feat(): Change IP

--- public/index.js
+++ public/index.js
@@ -14,8 +14,7 @@ firebase.initializeApp({
 var game = new Phaser.Game(WindowWidth, WindowHeight, Phaser.AUTO);
 
 // Add Socket to Game
-game.socket = io('http://localhost:80/Hall');
-game.socket = io('http://192.168.50.133:80/Hall');
+game.socket = io('http://34.80.102.213:80/Hall');
 
 // Add Game State
 game.state.add('Menu', new Menu());
