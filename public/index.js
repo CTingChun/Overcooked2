4b722a2367e524d4ceb2ca9b5330002025298667
Elven Lin <elvenlin1998@gmail.com>
2019-06-22 08:46:36 +0800

    fd

--- public/index.js
+++ public/index.js
@@ -14,7 +14,7 @@ firebase.initializeApp({
 var game = new Phaser.Game(WindowWidth, WindowHeight, Phaser.AUTO);
 
 // Add Socket to Game
-game.socket = io('http://34.80.102.213:8000/Hall');
+game.socket = io('https://final-project-team31.herokuapp.com/Hall');
 
 // Add Game State
 game.state.add('Menu', new Menu());
